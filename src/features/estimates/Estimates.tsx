412b37a258fc8fcf0119d4790dffa625c6905466
Vitomir Budimir <budimirvitomir@gmail.com>
2021-09-28 09:44:13 +0200

    feat: add Estimates loader overlay

--- src/features/estimates/Estimates.tsx
+++ src/features/estimates/Estimates.tsx
@@ -1,10 +1,28 @@
-import { Container, Typography } from '@material-ui/core';
+import {
+  Backdrop,
+  CircularProgress,
+  Container,
+  Typography,
+} from '@material-ui/core';
 import { useTheme } from '@material-ui/core/styles';
 
 import { AddEstimateForm, EstimatesOverview } from './components/';
+import { useAuthenticateQuery } from './estimatesApi';
+
+function LoadingOverlay() {
+  return (
+    <Backdrop open={true}>
+      <CircularProgress color="inherit" />
+    </Backdrop>
+  );
+}
 
 export function Estimates() {
+  const { isLoading: isAuthenticating } = useAuthenticateQuery(null);
+
   const theme = useTheme();
+
+  if (isAuthenticating) return <LoadingOverlay />;
 
   return (
     <Container style={{ paddingBottom: theme.spacing(2) }}>
