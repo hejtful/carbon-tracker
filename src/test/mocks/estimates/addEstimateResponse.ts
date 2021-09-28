df1b5f3aef72ec3cb02ee627a136014196a7b592
Vitomir Budimir <budimirvitomir@gmail.com>
2021-09-28 18:59:30 +0200

    test: add mocks

---
+++ src/test/mocks/estimates/addEstimateResponse.ts
@@ -0,0 +1,17 @@
+export const mockAddEstimateResponse = {
+  data: {
+    id: 'ba324b1e-238a-484d-aac6-a0c9a707864f',
+    type: 'estimate',
+    attributes: {
+      country: 'fr',
+      state: null,
+      electricity_unit: 'mwh',
+      electricity_value: 61,
+      estimated_at: '2021-09-28T14:51:16.144Z',
+      carbon_g: 3355000,
+      carbon_lb: 7396.51,
+      carbon_kg: 3355,
+      carbon_mt: 3.36,
+    },
+  },
+};
